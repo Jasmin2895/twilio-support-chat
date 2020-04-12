22c781d1f0700b11d216aea3d7b32cbfcfb0095d
buckleader <jasmin28virdi@gmail.com>
2020-04-13 00:50:02 +0530

    modified package.json to run both server and client together

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();  //better server logging
 
 const router = require('./src/router');
 const syncServiceDetails = require('./src/sync_service_details');
@@ -16,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add body parser for Notify device registration
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(pino);
 
 app.use(router);
 
@@ -24,7 +26,7 @@ syncServiceDetails();
 
 // Create http server and run it
 const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 server.listen(port, function() {
   console.log('Express server running on *:' + port);
 });
