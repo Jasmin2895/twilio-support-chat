8607b9a6b8d618957bf97c0f41f1009575635c87
buckleader <jasmin28virdi@gmail.com>
2020-04-14 00:00:02 +0530

    modified app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import $ from jQuery;
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state= {
+      messages:[],
+      username: null
+    }
+  }
+  componentDidMount = () => {
+    this.getToken()
+    .catch((error)=> {
+      this.setState({
+        messages: [...this.state.messages, {body: `Error: ${error.message}`}]
+      })
+    })
+  }
+  
+  getToken = () => {
+    return new Promise((resolve, reject)=> {
+      this.setState({messages: [...this.state.messages, {body: `Connecting...`}]})
+    
+      $.getJSON('/token', (token)=> {
+        this.setState({username: token.identity})
+        resolve(token)
+      }).fail(()=> {
+        reject(Error("failed to connect..."))
+      })
+    })
   }
   render() {
     return (
@@ -23,3 +49,5 @@ class App extends Component {
     );
   }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById("container"));
+ReactDOM.render(<App />, document.getElementById("container"));
