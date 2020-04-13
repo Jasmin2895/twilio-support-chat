cd9a95322c62efcee798e1e2eaf6aec7e6fd0ab4
buckleader <jasmin28virdi@gmail.com>
2020-04-14 01:02:04 +0530

    code modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,31 @@
 import React, { Component } from "react";
-import $ from "jquery";
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import "./../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       messages: [],
-      username: null,
     };
   }
-  componentDidMount = () => {
-    this.getToken().catch((error) => {
-      this.setState({
-        messages: [...this.state.messages, { body: `Error: ${error.message}` }],
-      });
+
+  handleNewMessage = (text) => {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        { me: true, author: "Me", body: text },
+      ],
     });
   };
 
-  getToken = () => {
-    return new Promise((resolve, reject) => {
-      this.setState({
-        messages: [...this.state.messages, { body: `Connecting...` }],
-      });
-
-      $.getJSON("/token", (token) => {
-        this.setState({ username: token.identity });
-        resolve(token);
-      }).fail(() => {
-        reject(Error("failed to connect..."));
-      });
-    });
-  };
   render() {
     return (
-      <div className="chat">
-        <div className="chat-title">Chat Window</div>
-        <div className="chat-body">
-          <div id="messages"></div>
-          <div className="chat-msg">
-            <input
-              id="chat-input"
-              type="text"
-              placeholder="say anything"
-              autofocus
-            />
-          </div>
-        </div>
+      <div className="App">
+        App rendered
+        <MessageList messages={this.state.messages} />
+        <MessageForm onMessageSend={this.handleNewMessage} />
       </div>
     );
   }
