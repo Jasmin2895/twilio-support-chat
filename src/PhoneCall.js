cb3bb313deaadce60f9577f26886f5c57b060b3f
buckleader <jasmin28virdi@gmail.com>
2020-04-23 03:09:05 +0530

    dropdown for country code

--- src/PhoneCall.js
+++ src/PhoneCall.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Button } from "semantic-ui-react";
+import { Dropdown } from "semantic-ui-react";
+import { Flag, Segment } from "semantic-ui-react";
 import "./PhoneCall.css";
-
 class PhoneCall extends Component {
   constructor(props) {
     super(props);
@@ -12,33 +13,51 @@ class PhoneCall extends Component {
       muted: false,
       onPhone: false,
       countries: [
-        { name: "United States", cc: "1", code: "us" },
-        { name: "Great Britain", cc: "44", code: "gb" },
-        { name: "Colombia", cc: "57", code: "co" },
-        { name: "Ecuador", cc: "593", code: "ec" },
-        { name: "Estonia", cc: "372", code: "ee" },
-        { name: "Germany", cc: "49", code: "de" },
-        { name: "Hong Kong", cc: "852", code: "hk" },
-        { name: "Ireland", cc: "353", code: "ie" },
-        { name: "Singapore", cc: "65", code: "sg" },
-        { name: "Spain", cc: "34", code: "es" },
-        { name: "Brazil", cc: "55", code: "br" },
-        { name: "India", cc: "91", code: "br" },
+        {
+          text: "United States",
+          key: "1",
+          countryCode: "us",
+          value: "1",
+        },
+        { value: "44", text: "Great Britain", key: "44", icon: "gb flag" },
+        { value: "57", text: "Colombia", key: "57", icon: "co flag" },
+        { value: "593", text: "Ecuador", key: "593", icon: "ec flag" },
+        { value: "372", text: "Estonia", key: "372", icon: "ee flag" },
+        { value: "49", text: "Germany", key: "49", icon: "de flag" },
+        { value: "852", text: "Hong Kong", key: "852", icon: "hk flag" },
+        { value: "353", text: "Ireland", key: "353", icon: "ie flag" },
+        { value: "65", text: "Singapore", key: "65", icon: "sg flag" },
+        { value: "44", text: "Spain", key: "34", icon: "es flag" },
+        { value: "55", text: "Brazil", key: "55", icon: "br flag" },
+        { value: "91", text: "India", key: "91", icon: "in flag" },
       ],
       connection: null,
     };
   }
+
+  handleChange = (event, data) => {
+    console.log("event data", event, data);
+  };
   render() {
     return (
       <div className="container">
         <form>
           <label>Enter your Phone Number to schedule a call</label>
-          <input
-            type="tel"
-            id="phone_no"
-            name="firstname"
-            placeholder="Phone Number..."
-          ></input>
+          <div className="input_fields">
+            <Dropdown
+              placeholder="Select Country"
+              button
+              floating
+              className="button icon"
+              options={this.state.countries}
+            />
+            <input
+              type="tel"
+              id="phone_no"
+              name="firstname"
+              placeholder="Phone Number..."
+            ></input>
+          </div>
           <div className="phone_call_submit">
             <button className="ui primary button" type="submit">
               Submit
