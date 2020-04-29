9cf96090b579092cf73e5bd959825ae3229a7232
buckleader <jasmin28virdi@gmail.com>
2020-04-30 02:06:59 +0530

    removed unnecessary code from controllers

--- controllers/message.js
+++ controllers/message.js
@@ -27,32 +27,3 @@ exports.find = (req, res) => {
     res.send(message);
   });
 };
-
-// module.exports.create = function (req, res, next) {
-//   console.log("req&&&&&&&&&&&&&&&&&&&&&&&&&&&", req.body);
-//   let message = new messages({
-//     message: "This is automated text message",
-//     to: `${process.env.TWILIO_NUMBER}`,
-//     from: req.body.number,
-//   });
-
-//   let msgData = {
-//     message: "This is automated text message",
-//     to: `${process.env.TWILIO_NUMBER}`,
-//     from: req.body.number,
-//   };
-
-//   // message.save((err) => {
-//   //   console.log(err);
-//   //   if (err) {
-//   //     return next(err);
-//   //   }
-//   //   res.send("Message created successfully");
-//   // });
-
-//   message.insertOne(msgData, (err, res) => {
-//     if (err) throw err;
-
-//     console.log("document inserted");
-//   });
-// };
